37a74ce7725ecd551c247e0d75997dd767b56224
= <=>
2023-03-07 18:49:29 -0300

    subir carpeta con archivos funciones

---
+++ funciones/0-1funciones.py
@@ -0,0 +1,9 @@
+def hola(nombre, apellido='Cuestas Manthai'):
+    print('Hola Mundo')
+    print(f'Mi nombre es {nombre} {apellido}')
+
+
+hola('Leonardo')
+hola('Chanchito')
+
+hola(apellido='Cuestas Manthai', nombre='Leonardo')
