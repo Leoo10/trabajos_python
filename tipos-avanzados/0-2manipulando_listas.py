8d7e2c5b455f1d1d962ea133c11d3535e29e2022
= <=>
2023-03-08 11:12:27 -0300

    subir nuevos archivos de la carpeta tipos-avanzados

--- tipos-avanzados/0-2manipulando_listas.py
+++ tipos-avanzados/0-2manipulando_listas.py
@@ -1,10 +1,4 @@
-# mascotas = ['Wolfgang', 'Pelusa', 'Pulga', 'Copito']
-# print(mascotas)
-# print(mascotas[::2])
-# print(mascotas[1:2:2])
-
-misDatos = ['Leonardo', 'Cuestas Manthai', 22,
-            'argentino y alemán', 'Nicolai', 'Tüch']
-
-print(misDatos)
-print(misDatos[1:3:2])
+mascotas = ['Wolfgang', 'Pelusa', 'Pulga', 'Copito']
+print(mascotas)
+print(mascotas[::2])
+print(mascotas[1:2:2])
